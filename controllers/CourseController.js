35ef56673ec2859050e816b4c4ec46bc0540fdd2
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-03-25 14:40:02 +0600

    get course api : also get author data.

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -1,19 +1,24 @@
 const Course = require("../models/Course");
+const User = require("../models/User");
 
 const getCourse = async (req, res) => {
   const id = req.params.id;
-  const result = await Course.find({ _id: id });
+  const result = await Course.findOne({ _id: id });
+  const author_list = result?.course_author_list;
+  const authorsData = await User.find({ _id: { $in: author_list } });
   if (result) {
     res.send({
       status: true,
       acknowledge: true,
       courseData: result,
+      authorData: authorsData,
     });
   } else {
     res.send({
       status: true,
       acknowledge: false,
       courseData: result,
+      authorData: authorsData,
     });
   }
 };
